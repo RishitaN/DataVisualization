87e4a33d8d45349056eb5d8ee13e292dbdc15951
RishitaN <rishita.ntu@gmail.com>
2021-06-25 16:22:24 +0800

    Update

---
+++ grc.js
@@ -0,0 +1,273 @@
+function Click_Aljunied(){
+  document.getElementById("grc-result").innerHTML = "Aljunied";
+  document.getElementById("electorNUM").innerHTML = "151,007";
+  document.getElementById("votes-castNUM").innerHTML = "144,727";
+  document.getElementById("valid-votesNUM").innerHTML = "143,145";
+  document.getElementById("spoilt-votesNUM").innerHTML = "1,582";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "WP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "WP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "59.95%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "PAP.png";
+  document.getElementById("runner-partyGRC").innerHTML = "PAP";
+  document.getElementById("run-party-votesGRC").innerHTML = "40.05";
+  
+}
+
+function Click_AngMoKio(){
+  document.getElementById("grc-result").innerHTML = "Ang Mo Kio";
+  document.getElementById("electorNUM").innerHTML = "185,465";
+  document.getElementById("votes-castNUM").innerHTML = "178,283";
+  document.getElementById("valid-votesNUM").innerHTML = "173,274";
+  document.getElementById("spoilt-votesNUM").innerHTML = "5,009";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "PAP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "71.91%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "RP.png";
+  document.getElementById("runner-partyGRC").innerHTML = "RP";
+  document.getElementById("run-party-votesGRC").innerHTML = "28.09";  
+}
+
+function Click_BishanToaPayoh(){
+  document.getElementById("grc-result").innerHTML = "Bishan-Toa Payoh";
+  document.getElementById("electorNUM").innerHTML = "101,366";
+  document.getElementById("votes-castNUM").innerHTML = "95,722";
+  document.getElementById("valid-votesNUM").innerHTML = "93,679";
+  document.getElementById("spoilt-votesNUM").innerHTML = "2,043";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "PAP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "67.23%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "SPP.png";
+  document.getElementById("runner-partyGRC").innerHTML = "SPP";
+  document.getElementById("run-party-votesGRC").innerHTML = "32.77";  
+}
+
+function Click_ChuaChuKang(){
+  document.getElementById("grc-result").innerHTML = "Chua Chu Kang";
+  document.getElementById("electorNUM").innerHTML = "106,693";
+  document.getElementById("votes-castNUM").innerHTML = "102,976";
+  document.getElementById("valid-votesNUM").innerHTML = "101,566";
+  document.getElementById("spoilt-votesNUM").innerHTML = "1,410";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "PAP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "58.64%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "PSP.jpg";
+  document.getElementById("runner-partyGRC").innerHTML = "PSP";
+  document.getElementById("run-party-votesGRC").innerHTML = "41.36";
+  
+}
+function Click_EastCoast(){
+  document.getElementById("grc-result").innerHTML = "East Coast";
+  document.getElementById("electorNUM").innerHTML = "121,772";
+  document.getElementById("votes-castNUM").innerHTML = "115,912";
+  document.getElementById("valid-votesNUM").innerHTML = "114,519";
+  document.getElementById("spoilt-votesNUM").innerHTML = "1,393";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "PAP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "53.39%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "WP.png";
+  document.getElementById("runner-partyGRC").innerHTML = "WP";
+  document.getElementById("run-party-votesGRC").innerHTML = "46.61";
+  
+}
+function Click_HollandBukitTimah(){
+  document.getElementById("grc-result").innerHTML = "Holland-Bukit Timah";
+  document.getElementById("electorNUM").innerHTML = "115,012";
+  document.getElementById("votes-castNUM").innerHTML = "109,311";
+  document.getElementById("valid-votesNUM").innerHTML = "107,318";
+  document.getElementById("spoilt-votesNUM").innerHTML = "1,993";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "PAP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "SDP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "66.36%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "SDP.png";
+  document.getElementById("runner-partyGRC").innerHTML = "SDP";
+  document.getElementById("run-party-votesGRC").innerHTML = "33.64";
+  
+}
+function Click_JalanBesar(){
+  document.getElementById("grc-result").innerHTML = "Jalan Besar";
+  document.getElementById("electorNUM").innerHTML = "107,936";
+  document.getElementById("votes-castNUM").innerHTML = "101,835";
+  document.getElementById("valid-votesNUM").innerHTML = "98,892";
+  document.getElementById("spoilt-votesNUM").innerHTML = "2,943";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "PAP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "65.36%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "PV.png";
+  document.getElementById("runner-partyGRC").innerHTML = "PV";
+  document.getElementById("run-party-votesGRC").innerHTML = "34.64";
+  
+}
+function Click_Jurong(){
+  document.getElementById("grc-result").innerHTML = "Jurong";
+  document.getElementById("electorNUM").innerHTML = "131,234";
+  document.getElementById("votes-castNUM").innerHTML = "125,623";
+  document.getElementById("valid-votesNUM").innerHTML = "123,106";
+  document.getElementById("spoilt-votesNUM").innerHTML = "2,517";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "PAP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "74.61%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "RDU.jpg";
+  document.getElementById("runner-partyGRC").innerHTML = "RDU";
+  document.getElementById("run-party-votesGRC").innerHTML = "25.39";
+  
+}
+function Click_MarineParade(){
+  document.getElementById("grc-result").innerHTML = "Marine Parade";
+  document.getElementById("electorNUM").innerHTML = "139,738";
+  document.getElementById("votes-castNUM").innerHTML = "132,037";
+  document.getElementById("valid-votesNUM").innerHTML = "130,250";
+  document.getElementById("spoilt-votesNUM").innerHTML = "1,787";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "PAP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "57.74%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "WP.png";
+  document.getElementById("runner-partyGRC").innerHTML = "WP";
+  document.getElementById("run-party-votesGRC").innerHTML = "42.26";
+  
+}
+function Click_MarsilingYewTee(){
+  document.getElementById("grc-result").innerHTML = "Marsiling-Yew Tee";
+  document.getElementById("electorNUM").innerHTML = "117,176";
+  document.getElementById("votes-castNUM").innerHTML = "112,600";
+  document.getElementById("valid-votesNUM").innerHTML = "110,503";
+  document.getElementById("spoilt-votesNUM").innerHTML = "2,097";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "PAP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "63.18%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "SDP.png";
+  document.getElementById("runner-partyGRC").innerHTML = "SDP";
+  document.getElementById("run-party-votesGRC").innerHTML = "36.82";
+  
+}
+function Click_NeeSoon(){
+  document.getElementById("grc-result").innerHTML = "Nee Soon";
+  document.getElementById("electorNUM").innerHTML = "147,047";
+  document.getElementById("votes-castNUM").innerHTML = "141,638";
+  document.getElementById("valid-votesNUM").innerHTML = "139,439";
+  document.getElementById("spoilt-votesNUM").innerHTML = "2,199";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "PAP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "61.9%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "PSP.jpg";
+  document.getElementById("runner-partyGRC").innerHTML = "PSP";
+  document.getElementById("run-party-votesGRC").innerHTML = "38.1";
+  
+}
+function Click_PasirRisPunggol(){
+  document.getElementById("grc-result").innerHTML = "Pasir Ris-Punggol";
+  document.getElementById("electorNUM").innerHTML = "166,587";
+  document.getElementById("votes-castNUM").innerHTML = "160,708";
+  document.getElementById("valid-votesNUM").innerHTML = "157,316";
+  document.getElementById("spoilt-votesNUM").innerHTML = "3,392";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "PAP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "64.16%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "SDA.png";
+  document.getElementById("runner-partyGRC").innerHTML = "SDA";
+  document.getElementById("run-party-votesGRC").innerHTML = "23.67";
+  
+}
+function Click_Sembawang(){
+  document.getElementById("grc-result").innerHTML = "Sembawang";
+  document.getElementById("electorNUM").innerHTML = "147,876";
+  document.getElementById("votes-castNUM").innerHTML = "142,901";
+  document.getElementById("valid-votesNUM").innerHTML = "139,954";
+  document.getElementById("spoilt-votesNUM").innerHTML = "2,947";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "PAP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "67.29%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "NSP.png";
+  document.getElementById("runner-partyGRC").innerHTML = "NSP";
+  document.getElementById("run-party-votesGRC").innerHTML = "32.71";
+  
+}
+function Click_Senkang(){
+  document.getElementById("grc-result").innerHTML = "Senkang";
+  document.getElementById("electorNUM").innerHTML = "120,166";
+  document.getElementById("votes-castNUM").innerHTML = "116,730";
+  document.getElementById("valid-votesNUM").innerHTML = "115,536";
+  document.getElementById("spoilt-votesNUM").innerHTML = "1,194";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "WP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "WP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "52.12%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "PAP.png";
+  document.getElementById("runner-partyGRC").innerHTML = "PAP";
+  document.getElementById("run-party-votesGRC").innerHTML = "47.88";
+  
+}
+function Click_Tampines(){
+  document.getElementById("grc-result").innerHTML = "Tampines";
+  document.getElementById("electorNUM").innerHTML = "151,708";
+  document.getElementById("votes-castNUM").innerHTML = "146,059";
+  document.getElementById("valid-votesNUM").innerHTML = "142,543";
+  document.getElementById("spoilt-votesNUM").innerHTML = "3,516";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "PAP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "66.41%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "NSP.png";
+  document.getElementById("runner-partyGRC").innerHTML = "NSP";
+  document.getElementById("run-party-votesGRC").innerHTML = "33.59";
+  
+}
+function Click_TanjongPagar(){
+  document.getElementById("grc-result").innerHTML = "TanJong Pagar";
+  document.getElementById("electorNUM").innerHTML = "134,642";
+  document.getElementById("votes-castNUM").innerHTML = "126,067";
+  document.getElementById("valid-votesNUM").innerHTML = "124,137";
+  document.getElementById("spoilt-votesNUM").innerHTML = "1,930";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "PAP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "63.1%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "PSP.jpg";
+  document.getElementById("runner-partyGRC").innerHTML = "PSP";
+  document.getElementById("run-party-votesGRC").innerHTML = "36.9";
+  
+}
+function Click_WestCoast(){
+  document.getElementById("grc-result").innerHTML = "West Coast";
+  document.getElementById("electorNUM").innerHTML = "146,251";
+  document.getElementById("votes-castNUM").innerHTML = "140,299";
+  document.getElementById("valid-votesNUM").innerHTML = "138,654";
+  document.getElementById("spoilt-votesNUMNUM").innerHTML = "1,645";
+  
+  document.getElementById("winning-partyGRC-iconGRC").src  = "PAP.png";
+  document.getElementById("winning-partyGRC").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votesGRC").innerHTML = "51.68%";  
+  
+  document.getElementById("runner-partyGRC-iconGRC").src = "PSP.jpg";
+  document.getElementById("runner-partyGRC").innerHTML = "PSP";
+  document.getElementById("run-party-votesGRC").innerHTML = "48.32";
+  
+}
---
+++ smc.js
@@ -0,0 +1,224 @@
+function Click_BukitBatok(){
+  document.getElementById("smc-result").innerHTML = "BukitBatok";
+  document.getElementById("electors").innerHTML = "29,950";
+  document.getElementById("votes-cast").innerHTML = "28,820";
+  document.getElementById("valid-votes").innerHTML = "28,287";
+  document.getElementById("spoilt-votes").innerHTML = "533";
+  
+  document.getElementById("winning-party-icon").src  = "PAP.png";
+  document.getElementById("winning-party").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votes").innerHTML = "54.80%";  
+  
+  document.getElementById("runner-party-icon").src = "SDP.png";
+  document.getElementById("runner-party").innerHTML = "SDP";
+  document.getElementById("run-party-votes").innerHTML = "45.20";
+  
+}
+
+function Click_Bukitpanjang(){
+  document.getElementById("smc-result").innerHTML = "Bukit Panjang";
+  document.getElementById("electors").innerHTML = "35,497";
+  document.getElementById("votes-cast").innerHTML = "34,247";
+  document.getElementById("valid-votes").innerHTML = "33,661";
+  document.getElementById("spoilt-votes").innerHTML = "586";
+  
+  document.getElementById("winning-party-icon").src  = "PAP.png";
+  document.getElementById("winning-party").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votes").innerHTML = "53.73%";  
+  
+  document.getElementById("runner-party-icon").src = "SDP.png";
+  document.getElementById("runner-party").innerHTML = "SDP";
+  document.getElementById("run-party-votes").innerHTML = "46.27";  
+}
+
+function Click_Hougang(){
+  document.getElementById("smc-result").innerHTML = "Hougang";
+  document.getElementById("electors").innerHTML = "26,468";
+  document.getElementById("votes-cast").innerHTML = "25,514";
+  document.getElementById("valid-votes").innerHTML = "25,242";
+  document.getElementById("spoilt-votes").innerHTML = "272";
+  
+  document.getElementById("winning-party-icon").src  = "WP.png";
+  document.getElementById("winning-party").innerHTML = "WP - Winner";
+  document.getElementById("win-party-votes").innerHTML = "61.21%";  
+  
+  document.getElementById("runner-party-icon").src = "SDP.png";
+  document.getElementById("runner-party").innerHTML = "SDP";
+  document.getElementById("run-party-votes").innerHTML = "46.27";  
+}
+
+function Click_HongKah(){
+  document.getElementById("smc-result").innerHTML = "Hong Kah";
+  document.getElementById("electors").innerHTML = "28,071";
+  document.getElementById("votes-cast").innerHTML = "27,207";
+  document.getElementById("valid-votes").innerHTML = "26804";
+  document.getElementById("spoilt-votes").innerHTML = "403";
+  
+  document.getElementById("winning-party-icon").src  = "PAP.png";
+  document.getElementById("winning-party").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votes").innerHTML = "60.99%";  
+  
+  document.getElementById("runner-party-icon").src = "PSP.jpg";
+  document.getElementById("runner-party").innerHTML = "PSP";
+  document.getElementById("run-party-votes").innerHTML = "39.01%";  
+}
+
+function Click_KebunBaru(){
+  document.getElementById("smc-result").innerHTML = "Kebun Baru";
+  document.getElementById("electors").innerHTML = "22,653";
+  document.getElementById("votes-cast").innerHTML = "21,538";
+  document.getElementById("valid-votes").innerHTML = "21,151";
+  document.getElementById("spoilt-votes").innerHTML = "387";
+  
+  document.getElementById("winning-party-icon").src  = "PAP.png";
+  document.getElementById("winning-party").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votes").innerHTML = "62.92%";  
+  
+  document.getElementById("runner-party-icon").src = "PSP.jpg";
+  document.getElementById("runner-party").innerHTML = "PSP";
+  document.getElementById("run-party-votes").innerHTML = "37.08%";  
+}
+
+function Click_MountBatten(){
+  document.getElementById("smc-result").innerHTML = "MountBatten";
+  document.getElementById("electors").innerHTML = "24,267";
+  document.getElementById("votes-cast").innerHTML = "22,644";
+  document.getElementById("valid-votes").innerHTML = "22,060";
+  document.getElementById("spoilt-votes").innerHTML = "584";
+  
+  document.getElementById("winning-party-icon").src  = "PAP.png";
+  document.getElementById("winning-party").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votes").innerHTML = "73.82%";  
+  
+  document.getElementById("runner-party-icon").src = "PSP.jpg";
+  document.getElementById("runner-party").innerHTML = "PSP";
+  document.getElementById("run-party-votes").innerHTML = "26.18%";  
+}
+
+function Click_MacPherson(){
+  document.getElementById("smc-result").innerHTML = "MacPherson";
+  document.getElementById("electors").innerHTML = "28564";
+  document.getElementById("votes-cast").innerHTML = "27123";
+  document.getElementById("valid-votes").innerHTML = "26498";
+  document.getElementById("spoilt-votes").innerHTML = "625";
+  
+  document.getElementById("winning-party-icon").src  = "PAP.png";
+  document.getElementById("winning-party").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votes").innerHTML = "71.74%";  
+  
+  document.getElementById("runner-party-icon").src = "PPP.jpg";
+  document.getElementById("runner-party").innerHTML = "PPP";
+  document.getElementById("run-party-votes").innerHTML = "28.26%";  
+}
+
+function Click_Marymount(){
+  document.getElementById("smc-result").innerHTML = "MaryMount";
+  document.getElementById("electors").innerHTML = "23,444";
+  document.getElementById("votes-cast").innerHTML = "22,421";
+  document.getElementById("valid-votes").innerHTML = "22,116";
+  document.getElementById("spoilt-votes").innerHTML = "305";
+  
+  document.getElementById("winning-party-icon").src  = "PAP.png";
+  document.getElementById("winning-party").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votes").innerHTML = "55.04%";  
+  
+  document.getElementById("runner-party-icon").src = "PSP.jpg";
+  document.getElementById("runner-party").innerHTML = "PSP";
+  document.getElementById("run-party-votes").innerHTML = "44.96%";  
+}
+
+function Click_Pioneer(){
+  document.getElementById("smc-result").innerHTML = "Pioneer";
+  document.getElementById("electors").innerHTML = "24,672";
+  document.getElementById("votes-cast").innerHTML = "23,887";
+  document.getElementById("valid-votes").innerHTML = "23,537";
+  document.getElementById("spoilt-votes").innerHTML = "350";
+  
+  document.getElementById("winning-party-icon").src  = "PAP.png";
+  document.getElementById("winning-party").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votes").innerHTML = "62.00%";  
+  
+  document.getElementById("runner-party-icon").src = "PSP.jpg";
+  document.getElementById("runner-party").innerHTML = "PSP";
+  document.getElementById("run-party-votes").innerHTML = "35.22%";  
+}
+
+function Click_PotongPasir(){
+  document.getElementById("smc-result").innerHTML = "Potong Pasir";
+  document.getElementById("electors").innerHTML = "19,740";
+  document.getElementById("votes-cast").innerHTML = "18,844";
+  document.getElementById("valid-votes").innerHTML = "18,566";
+  document.getElementById("spoilt-votes").innerHTML = "278";
+  
+  document.getElementById("winning-party-icon").src  = "PAP.png";
+  document.getElementById("winning-party").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votes").innerHTML = "60.67%";  
+  
+  document.getElementById("runner-party-icon").src = "SPP.png";
+  document.getElementById("runner-party").innerHTML = "SPP";
+  document.getElementById("run-party-votes").innerHTML = "39.33%";  
+}
+
+function Click_PunggolWest(){
+  document.getElementById("smc-result").innerHTML = "Punggol West";
+  document.getElementById("electors").innerHTML = "26,579";
+  document.getElementById("votes-cast").innerHTML = "25,888";
+  document.getElementById("valid-votes").innerHTML = "25,672";
+  document.getElementById("spoilt-votes").innerHTML = "216";
+  
+  document.getElementById("winning-party-icon").src  = "PAP.png";
+  document.getElementById("winning-party").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votes").innerHTML = "60.98%";  
+  
+  document.getElementById("runner-party-icon").src = "WP.png";
+  document.getElementById("runner-party").innerHTML = "WP";
+  document.getElementById("run-party-votes").innerHTML = "39.02%";  
+}
+
+function Click_RadinMas(){
+  document.getElementById("smc-result").innerHTML = "Radin Mas";
+  document.getElementById("electors").innerHTML = "24,980";
+  document.getElementById("votes-cast").innerHTML = "23,603";
+  document.getElementById("valid-votes").innerHTML = "22,786";
+  document.getElementById("spoilt-votes").innerHTML = "817";
+  
+  document.getElementById("winning-party-icon").src  = "PAP.png";
+  document.getElementById("winning-party").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votes").innerHTML = "74.01%";  
+  
+  document.getElementById("runner-party-icon").src = "RP.png";
+  document.getElementById("runner-party").innerHTML = "RP";
+  document.getElementById("run-party-votes").innerHTML = "25.99%";  
+}
+
+function Click_YioChuKang(){
+  document.getElementById("smc-result").innerHTML = "Yio Chu kang";
+  document.getElementById("electors").innerHTML = "26,005";
+  document.getElementById("votes-cast").innerHTML = "24,707";
+  document.getElementById("valid-votes").innerHTML = "24,294";
+  document.getElementById("spoilt-votes").innerHTML = "413";
+  
+  document.getElementById("winning-party-icon").src  = "PAP.png";
+  document.getElementById("winning-party").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votes").innerHTML = "60.82%";  
+  
+  document.getElementById("runner-party-icon").src = "PSP.jpg";
+  document.getElementById("runner-party").innerHTML = "PSP";
+  document.getElementById("run-party-votes").innerHTML = "39.18%";  
+}
+
+function Click_Yuhua(){
+  document.getElementById("smc-result").innerHTML = "Yuhua";
+  document.getElementById("electors").innerHTML = "21,376";
+  document.getElementById("votes-cast").innerHTML = "20,438";
+  document.getElementById("valid-votes").innerHTML = "20,032";
+  document.getElementById("spoilt-votes").innerHTML = "406";
+  
+  document.getElementById("winning-party-icon").src  = "PAP.png";
+  document.getElementById("winning-party").innerHTML = "PAP - Winner";
+  document.getElementById("win-party-votes").innerHTML = "70.54%";  
+  
+  document.getElementById("runner-party-icon").src = "SDP.png";
+  document.getElementById("runner-party").innerHTML = "SDP";
+  document.getElementById("run-party-votes").innerHTML = "29.46%";  
+}
